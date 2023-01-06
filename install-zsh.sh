8d775167b2e3d3b56c0e0b891762ce57feb7d996
Dustin Krysak <dustin@bashfulrobot.com>
2023-01-05 18:07:48 -0800

    fix: :bug: isntall zimfw proper

--- install-zsh.sh
+++ install-zsh.sh
@@ -11,15 +11,16 @@ curl -L -o starship-x86_64-unknown-linux-gnu.tar.gz ${DL_URL}
 tar xvfz starship-x86_64-unknown-linux-gnu.tar.gz
 rm -f starship-x86_64-unknown-linux-gnu.tar.gz
 sudo install starship /usr/local/bin
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 cd $HOME
 curl -L -o .zshrc https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.zshrc
 curl -L -o .zimrc https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.zimrc
 cd $HOME/.config
 curl -L -o starship.toml https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.config/starship.toml
-cd $HOME/.zim
-curl -L -o .latest_version https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.zim/.latest_version
-curl -L -o init.zsh https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.zim/init.zsh
-curl -L -o zimfw.zsh https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.zim/zimfw.zsh
+# cd $HOME/.zim
+# curl -L -o .latest_version https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.zim/.latest_version
+# curl -L -o init.zsh https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.zim/init.zsh
+# curl -L -o zimfw.zsh https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.zim/zimfw.zsh
 cd $HOME/.config/zsh
 curl -L -o do-update https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.config/zsh/do-update
 curl -L -o runAptUpdateIfNeeded https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.config/zsh/runAptUpdateIfNeeded
