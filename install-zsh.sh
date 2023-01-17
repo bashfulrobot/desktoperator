b3db60e53b25d8049ccaa1ef92a668ad24249830
Dustin Krysak <dustin@bashfulrobot.com>
2023-01-16 16:29:53 -0800

    chore: :art: update shell to use p10k vs starship Changes to be committed: 	modified:   install-zsh.sh

--- install-zsh.sh
+++ install-zsh.sh
@@ -5,8 +5,8 @@ sudo apt update && sudo apt install curl git fonts-powerline jq zsh neovim -y
 sleep 5
 mkdir -p $HOME/.config/zsh
 
-# Install starship prompt
-STARSHIP_TAG=$(curl -sL https://api.github.com/repos/starship/starship/releases/latest | jq -r ".tag_name") &&DL_URL=$(echo "https://github.com/starship/starship/releases/download/${STARSHIP_TAG}/starship-x86_64-unknown-linux-gnu.tar.gz") && curl -L -o starship-x86_64-unknown-linux-gnu.tar.gz ${DL_URL} && tar xvfz starship-x86_64-unknown-linux-gnu.tar.gz && rm -f starship-x86_64-unknown-linux-gnu.tar.gz && sudo install starship /usr/local/bin && cd $HOME/.config && curl -L -o starship.toml https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.config/starship.toml
+# # Install starship prompt
+# STARSHIP_TAG=$(curl -sL https://api.github.com/repos/starship/starship/releases/latest | jq -r ".tag_name") &&DL_URL=$(echo "https://github.com/starship/starship/releases/download/${STARSHIP_TAG}/starship-x86_64-unknown-linux-gnu.tar.gz") && curl -L -o starship-x86_64-unknown-linux-gnu.tar.gz ${DL_URL} && tar xvfz starship-x86_64-unknown-linux-gnu.tar.gz && rm -f starship-x86_64-unknown-linux-gnu.tar.gz && sudo install starship /usr/local/bin && cd $HOME/.config && curl -L -o starship.toml https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.config/starship.toml
 
 # Install fwzim
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
@@ -26,16 +26,24 @@ curl -L -o runAptUpdateIfNeeded https://raw.githubusercontent.com/bashfulrobot/d
 curl -L -o echoHeader https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.config/zsh/echoHeader
 curl -L -o echoSection https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.config/zsh/echoSection
 
+# Add Prompt config
+cd $HOME/
+curl -Lo .p10k.zsh https://raw.githubusercontent.com/bashfulrobot/desktoperator/main/files/home/.p10k.zsh
+cd $HOME/.config/zsh
+
 # sudo ln -s /usr/bin/batcat /usr/bin/bat
 
-# Add customer .zshrc cfg
+# Add custom .zshrc cfg
 echo '### DK' >> $HOME/.zshrc
 echo '######=== Autoload functions.' >> $HOME/.zshrc
 echo 'fpath=( $HOME/.config/zsh "${fpath[@]}" )' >> $HOME/.zshrc
 echo 'autoload -Uz $fpath[1]/*(.:t)' >> $HOME/.zshrc
 echo '' >> $HOME/.zshrc
-echo '# Enable starship prompt' >> $HOME/.zshrc
-echo 'eval "$(starship init zsh)"' >> $HOME/.zshrc
+# echo '# Enable starship prompt' >> $HOME/.zshrc
+# echo 'eval "$(starship init zsh)"' >> $HOME/.zshrc
+echo '# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.' >> $HOME/.zshrc
+echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> $HOME/.zshrc
+
 
 # Add customer .zimrc cfg
 echo '### DK Config' >> $HOME/.zimrc
@@ -47,8 +55,10 @@ echo '# k9s' >> $HOME/.zimrc
 echo 'zmodule joke/zim-k9s' >> $HOME/.zimrc
 echo '# kubectl' >> $HOME/.zimrc
 echo 'zmodule joke/zim-kubectl' >> $HOME/.zimrc
-echo '# starship' >> $HOME/.zimrc
-echo 'zmodule joke/zim-starship' >> $HOME/.zimrc
+# echo '# starship' >> $HOME/.zimrc
+# echo 'zmodule joke/zim-starship' >> $HOME/.zimrc
+echo '# powerlevel10k' >> $HOME/.zimrc
+echo 'zmodule romkatv/powerlevel10k --use degit' >> $HOME/.zimrc
 
 sleep 15
 
