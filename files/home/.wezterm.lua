67150b0bda8b451d7beb0ab18c4f6e841e10cae4
Dustin Krysak <dustin@bashfulrobot.com>
2023-02-27 10:14:14 -0800

    feat: :sparkles: added wezterm support
    - looks great, renders my prompt properly
    
    Changes to be committed:
    	new file:   files/home/.config/wezterm/colors/dracula.toml
    	new file:   files/home/.wezterm.lua
    	new file:   tasks/desktop/software/wezterm.yaml

---
+++ files/home/.wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require 'wezterm'
+
+return {
+  
+  font = wezterm.font 'Berkeley Mono Variable',
+  font_size = 18.0,
+
+  cursor_blink_rate = 800,
+
+  color_scheme = "Dracula (Official)",
+  tab_bar_at_bottom = true,
+  use_fancy_tab_bar = false,
+  window_decorations = "RESIZE"
+}
+
+
